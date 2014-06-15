e2aa8db9658c900164e5a3f2ef21977897ade780
AlexanderHenry <alexanderhenry21@gmail.com>
2014-06-15 13:00:16 -0500

    Add number_prompt.rb

---
+++ week_1/number_prompt.rb
@@ -0,0 +1,12 @@
+puts "Please enter the first number"
+first_number = gets.chop
+
+puts "Please enter the second number"
+second_number = gets.chop
+
+puts "Please enter the third number"
+third_number = gets.chop
+
+total = first_number + second_number + third_number
+
+puts "The total of " + first_number.to_s + ", " + second_number.to_s + ", and " + third_number.to_s + " = " total.to_s

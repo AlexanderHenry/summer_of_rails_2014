434f7e0fad07c04fbb3811b518dd11b64eabc766
Brian Hogan / New Auburn Personal Computer Services <bphogan@gmail.com>
2014-06-15 21:49:56 -0500

    comments on number_prompt

--- week_1/number_prompt.rb
+++ week_1/number_prompt.rb
@@ -9,4 +9,5 @@ third_number = gets.chop
 
 total = first_number + second_number + third_number
 
+#FIXME: you should use string interpolation for this instead of using +
 puts "The total of " + first_number.to_s + ", " + second_number.to_s + ", and " + third_number.to_s + " = " total.to_s

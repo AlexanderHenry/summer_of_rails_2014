fb4a8fdf7ee67291a5bed0ab3a135a43efca1ccc
Alex Henry <alexanderhenry21@gmail.com>
2014-07-05 17:24:10 +0000

    Added return_values.rb

---
+++ week_1/return_values.rb
@@ -0,0 +1,24 @@
+def add_first
+  1+1
+  2+2
+  3+3
+end
+
+def add_second
+  a = 1
+  b = 2
+  c = a + b
+  d = 4
+  c
+end
+
+def bonus
+  a = 1
+  b = 2
+  return a
+  c = a + b
+end
+
+puts add_first
+puts add_second
+puts bonus

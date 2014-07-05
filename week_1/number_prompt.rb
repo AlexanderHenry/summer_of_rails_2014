77281cfd6a6afba0bfc6990a9c12f59225740ebc
Alex Henry <alexanderhenry21@gmail.com>
2014-07-05 17:15:42 +0000

    Added string interpolation to number_prompt

--- week_1/number_prompt.rb
+++ week_1/number_prompt.rb
@@ -7,6 +7,6 @@ second_number = gets.chop
 puts "Please enter the third number"
 third_number = gets.chop
 
-total = first_number + second_number + third_number
+total = first_number.to_i + second_number.to_i + third_number.to_i
 
 puts "The total of #{first_number}, #{second_number}, and #{third_number} = #{total}."

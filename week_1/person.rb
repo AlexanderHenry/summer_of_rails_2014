22f5ae8781ca0549f788439ff502fc25744e61e5
Alex Henry <alexanderhenry21@gmail.com>
2014-07-06 00:31:14 +0000

    Updated person.rb to list entire array when app ends

--- week_1/person.rb
+++ week_1/person.rb
@@ -14,6 +14,12 @@ def collectInfo(first_name = "Homer", last_name = "Simpson", age = 40)
   puts "Would you like to add another person to the people array? Please type yes or no"
   answer = gets.chop
   collectInfo if answer == "yes"
-  puts "You entered #{people.length + 1} people into the array" if answer == "no"
+  #people.length is always 1 - due to it being inside the function...
+  puts "You entered #{people.length} person(s) into the array" if answer == "no"
+    
+  person.each do |key, value|
+    puts "#{key}: #{value}\n"
+  end
 end
+
 collectInfo
